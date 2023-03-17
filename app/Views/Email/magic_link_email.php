d78929f466cdee40767de46ef1a5096ec8dc7893
nayrouz0041 <nayrouzbouzidi0041@gmail.com>
2023-03-17 16:34:15 -0300

    Update_magic_link_inEmail
    link_inEmail View

--- app/Views/Email/magic_link_email.php
+++ app/Views/Email/magic_link_email.php
@@ -1,37 +1,211 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta name="x-apple-disable-message-reformatting">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="format-detection" content="telephone=no, date=no, address=no, email=no">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title><?= lang('Auth.magicLinkSubject') ?></title>
+
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
+    <link rel="shortcut icon" href="<?php echo base_url('favicon.png'); ?>" type="image/x-icon" />
+
+
+    <style type="text/css">
+        body{width: 100%; background-color: #333333; margin:0; padding:0; -webkit-font-smoothing: antialiased;mso-margin-top-alt:0px; mso-margin-bottom-alt:0px; mso-padding-alt: 0px 0px 0px 0px;}
+        p,h1,h2,h3,h4{margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;}
+        span.preheader{display: none; font-size: 1px;}
+        html{width: 100%;}
+        table{font-size: 12px;border: 0;}
+        .menu-space{padding-right:25px;}
+        a,a:hover { text-decoration:none; color:#FFF;}
+
+        @media only screen and (max-width:640px) {
+            body {width:auto!important;}
+            table [class=main] {width:440px !important;}
+            table [class=two-left] {width:420px !important; margin:0px auto;}
+            table [class=full] {width:100% !important; margin:0px auto;}
+            table [class=two-left-inner] {width:400px !important; margin:0px auto;}
+            table [class=menu-icon] { display:none;}
+        }
+
+        @media only screen and (max-width:479px) {
+            body {width:auto!important;}
+            table [class=main]  {width:310px !important;}
+            table [class=two-left] {width:300px !important; margin:0px auto;}
+            table [class=full] {width:100% !important; margin:0px auto;}
+            table [class=two-left-inner] {width:280px !important; margin:0px auto;}
+            table [class=menu-icon] { display:none;}
+        }
+    </style>
 </head>
+<body yahoo="fix" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+    <!--Main Table Start-->
+    <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#333333">
+        <tr>
+            <td align="center" valign="middle">
+                <!-- Top space -->
+                <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td align="center" valign="middle">
+                            <table width="450" border="0" align="center" cellpadding="0" cellspacing="0" class="main">
+                                <tr>
+                                    <td height="90" align="center" valign="top" style="font-size:90px; line-height:90px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+                <!-- / Top space -->
 
-<body>
-    <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-radius: 6px; border-collapse: separate !important;">
-        <tbody>
-            <tr>
-                <td style="line-height: 24px; font-size: 16px; border-radius: 6px; margin: 0;" align="center" bgcolor="#0d6efd">
-                    <a href="<?= url_to('verify-magic-link') ?>?token=<?= $token ?>" style="color: #ffffff; font-size: 16px; font-family: Helvetica, Arial, sans-serif; text-decoration: none; border-radius: 6px; line-height: 20px; display: inline-block; font-weight: normal; white-space: nowrap; background-color: #0d6efd; padding: 8px 12px; border: 1px solid #0d6efd;"><?= lang('Reset Password') ?></a>
-                </td>
+                <!-- Header -->
+                <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td align="center" valign="middle">
+                            <table width="450" border="0" align="center" cellpadding="0" cellspacing="0" class="main">
+                                <tr>
+                                    <td align="center" valign="top" bgcolor="#03b0ef" style="-moz-border-radius: 25px 25px 0px 0px; border-radius: 25px 25px 0px 0px;">
+                                        <table width="450" border="0" align="center" cellpadding="0" cellspacing="0" class="two-left-inner">
+                                            <tr>
+                                                <td height="35" align="center" valign="top" style="font-size:35px; line-height:35px;">&nbsp;</td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" valign="top">
+                                                    <table border="0" align="center" cellpadding="0" cellspacing="0">
+                                                        <tr>
+                                                            <td align="center" valign="middle"><a href="<?php echo site_url('/'); ?>"><img editable="true" mc:edit="bm12-01" src="<?php echo base_url('assets/img/logo_b.png'); ?>" width="175" height="40" alt="" /></a></td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td height="20" align="center" valign="top" style="font-size:20px; line-height:20px;">&nbsp;</td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+                <!-- / Header -->
 
-                <td style="line-height: 24px; font-size: 16px; border-radius: 6px; margin: 0;" align="center" bgcolor="#0d6efd">
-                    <a href="<?= url_to('verify-magic-link') ?>?token=<?= $token ?>" style="color: #ffffff; font-size: 16px; font-family: Helvetica, Arial, sans-serif; text-decoration: none; border-radius: 6px; line-height: 20px; display: inline-block; font-weight: normal; white-space: nowrap; background-color: #0d6efd; padding: 8px 12px; border: 1px solid #0d6efd;"><?= lang('Reset Password') ?></a>
-                </td>
-            </tr>
-        </tbody>
+                <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td align="center" valign="middle">
+                            <table width="450" border="0" align="center" cellpadding="0" cellspacing="0" class="main">
+                                <tr>
+                                    <td align="center" valign="top" bgcolor="#FFFFFF">
+                                        <table width="350" border="0" align="center" cellpadding="0" cellspacing="0" class="two-left">
+                                            <tr>
+                                                <td height="70" align="center" valign="top" style="font-size:70px; line-height:70px;">&nbsp;</td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" valign="top">
+                                                    <table width="70" border="0" align="center" cellpadding="0" cellspacing="0">
+                                                        <tr>
+                                                            <td align="center" valign="top"><img editable="true" mc:edit="bm16-02" style="border-radius:50%;" src="" width="83" height="83" alt="" /></td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" valign="top">
+                                                    <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                                                        <tr>
+                                                            <td height="10" align="center" valign="top" style="font-size:10px; line-height:10px;">&nbsp;</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="center" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:18px; color:#000000; font-weight:normal; line-height:40px;" mc:edit="bm16-03"><multiline>Olá, <?php echo "nome"; ?></multiline></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td height="5" align="center" valign="top" style="font-size:5px; line-height:5px;">&nbsp;</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="center" valign="top" style="font-family:'Open Sans', sans-serif, Verdana; font-size:15px; color:#4c4c4c; font-weight:normal; line-height:24px;" mc:edit="bm16-04"><multiline>Você solicitou uma nova senha no site do Easymeter. Para concluir esta operação, basta clicar no botão abaixo.</multiline></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td height="20" align="center" valign="top">&nbsp;</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="center" valign="top">
+                                                                <table width="190" border="0" align="center" cellpadding="0" cellspacing="0">
+                                                                    <tr>
+                                                                        <td height="50" align="center" valign="middle" bgcolor="#03b0ef" style="font-family:Arial, Helvetica, sans-serif; font-size:16px; color:#FFF; font-weight:bold; -moz-border-radius: 30px; border-radius: 30px;" mc:edit="bm16-05"><multiline><a href="<?=url_to('verify-magic-link'); ?>" style="text-decoration:none; color:#FFF;">Redefinir Senha</a></multiline></td>
+                                                                    </tr>
+                                                                </table>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td height="60" align="center" valign="top" style="font-size:60px; line-height:60px;">&nbsp;</td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+
+                <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td align="center" valign="middle">
+                            <table width="450" border="0" align="center" cellpadding="0" cellspacing="0" class="main">
+                                <tr>
+                                    <td align="center" valign="top" bgcolor="#03b0ef" style="font-family:'Open Sans', sans-serif, Verdana; font-size:12px; color:#FFF; font-weight:normal; line-height:20px; padding:30px 50px; -moz-border-radius:0px 0px 25px 25px; border-radius:0px 0px 25px 25px;" mc:edit="bm16-06"><multiline>Caso não tenha feito a solicitação, você não precisa tomar nenhuma ação e pode desconsiderar este e-⁠mail com segurança.</multiline></td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+
+                <!-- Footer -->
+                <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td align="center" valign="middle">
+                            <table width="600" border="0" align="center" cellpadding="0" cellspacing="0" class="main">
+                                <tr>
+                                    <td align="center" valign="top">
+                                        <table width="510" border="0" align="center" cellpadding="0" cellspacing="0" class="two-left-inner">
+                                            <tr><td height="35" align="center" valign="top" style="font-size:35px; line-height:35px;">&nbsp;</td></tr>
+                                            <tr>
+                                                <td align="center" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#FFF; font-weight:normal; line-height:28px;" mc:edit="bm12-18">
+                                                    <multiline>Easymeter - UNO Robótica Ltda.</multiline>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:14px; color:#FFF; font-weight:bold; line-height:28px;" mc:edit="bm12-19">
+                                                    <multiline>Copyright &copy; 2018 UNO Robótica </multiline>
+                                                </td>
+                                            </tr>
+                                    </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+                <!-- / Footer -->
+
+                <!-- Footer -->
+                <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td align="center" valign="middle">
+                            <table width="600" border="0" align="center" cellpadding="0" cellspacing="0" class="main">
+                                <tr>
+                                    <td height="90" align="center" valign="top" style="font-size:90px; line-height:90px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+                <!-- / Footer -->
+            </td>
+        </tr>
     </table>
-    <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
-        <tbody>
-            <tr>
-                <td style="line-height: 20px; font-size: 20px; width: 100%; height: 20px; margin: 0;" align="left" width="100%" height="20">
-                    &#160;
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    
+    <!--Main Table End-->
 </body>
-
 </html>

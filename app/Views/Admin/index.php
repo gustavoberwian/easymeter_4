904780fde9619fa9d6c205baaafd09d247644516
Rafael Assis <60201602+RafaAssis06@users.noreply.github.com>
2023-06-13 09:28:30 -0300

    Fix index

--- app/Views/Admin/index.php
+++ app/Views/Admin/index.php
@@ -10,7 +10,7 @@
             if ($atual == $central->entidade) {
                 $primeiro = "";
             } else {
-                $primeiro = "card-featured-left card-featured-primary";
+                $primeiro = "card-featured-start card-featured-primary";
                 $atual = $central->entidade;
             }
 
@@ -21,18 +21,18 @@
                         <a style="text-decoration: none;" href="<?= site_url('admin/centrais/' . $central->nome); ?>">
                             <h4 class="my-0 text-primary">
                                 <?= $central->nome; ?>
-                                <i class="card-body-online float-right fas fa-circle <?= format_online_status($central->ultimo_envio, $central->nome); ?>" title="<?= ($central->auto_ok > 0) ? '' : 'Processamento das leituras desabilitado'; ?>"></i>
+                                <i class="card-body-online float-end fas fa-circle <?= format_online_status($central->ultimo_envio, $central->nome); ?>" title="<?= ($central->auto_ok > 0) ? '' : 'Processamento das leituras desabilitado'; ?>"></i>
                                 <?php if (!is_null($central->tensao)) { ?>
-                                    <i class="card-body-online float-right mr-2 fas <?= ($central->fonte == "R") ? 'fa-bolt text-success' : 'fa-car-battery text-danger'; ?>" title="<?= number_format($central->tensao / 10, 1, ",", "") . 'V'; ?>"></i>
+                                    <i class="card-body-online float-end mr-2 fas <?= ($central->fonte == "R") ? 'fa-bolt text-success' : 'fa-car-battery text-danger'; ?>" title="<?= number_format($central->tensao / 10, 1, ",", "") . 'V'; ?>"></i>
                                 <?php } ?>
                                 <?php if (!is_null($central->fraude_hi) && ($central->fraude_hi != '000.000.000.000' || $central->fraude_low != '000.000.000.000')) { ?>
-                                    <i class="card-body-online float-right mr-2 fas fa-user-secret text-danger" title="Fraude Detectada"></i>
+                                    <i class="card-body-online float-end mr-2 fas fa-user-secret text-danger" title="Fraude Detectada"></i>
                                 <?php } ?>
                                 <?php if (!is_null($central->tamanho) && $central->tamanho > 0) { ?>
-                                    <i class="card-body-online float-right mr-2 fas fa-sort-amount-up text-<?= ($central->tamanho > 1000) ? "danger" : 'warning'; ?>" title="<?= number_format($central->tamanho, 0, ",", ".") . ' B'; ?>"></i>
+                                    <i class="card-body-online float-end mr-2 fas fa-sort-amount-up text-<?= ($central->tamanho > 1000) ? "danger" : 'warning'; ?>" title="<?= number_format($central->tamanho, 0, ",", ".") . ' B'; ?>"></i>
                                 <?php } ?>
                             </h4>
-                            <p class="text-3 text-muted my-0"><span title="Último envio"><?= ($central->ultimo_envio) ? date('d/m/y H:i:s', $central->ultimo_envio) : '-'; ?></span><span class="float-right" title="Localizador"><?= $central->localizador; ?></span></p>
+                            <p class="text-3 text-muted my-0"><span title="Último envio"><?= ($central->ultimo_envio) ? date('d/m/y H:i:s', $central->ultimo_envio) : '-'; ?></span><span class="float-end" title="Localizador"><?= $central->localizador; ?></span></p>
                             <div class="row">
                                 <div class="col-8">
                                     <p class="text-2 text-muted my-0"><?= substr($central->entidade, 0, 20); ?></p>
